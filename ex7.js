9caacf0589330e9fd3c1d7f6247d833b29a32be1
alexandredasilvaarruda23-ctrl <alexandredasilvaarruda23@gmail.com>
2026-03-09 22:17:51 -0300

    pw

---
+++ ex7.js
@@ -0,0 +1,7 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let maçã = 0;
+
+for (let batata = 0; batata < array.length; batata++) {
+  maçã += array[batata];
+}
+console.log(maçã);
---
+++ ex8.js
@@ -0,0 +1,2 @@
+var cachorrinho = [5, 6, 2, 7 ,3, 8, 1, 9, 4,];
+console.log(cachorrinho[]); // funciona corretamente
