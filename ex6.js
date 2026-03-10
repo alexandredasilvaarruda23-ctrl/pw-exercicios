377ff103265073b3bd8a6934b33baff921dc7232
alexandredasilvaarruda23-ctrl <alexandredasilvaarruda23@gmail.com>
2026-03-09 21:53:54 -0300

    pw

--- ex6.js
+++ ex6.js
@@ -1,6 +1,7 @@
-let i = 12
+let tabuada = 12
 let mult = 1
-while (mult<11){
-    console.log("Valor de i: " + i*mult);
+while (mult<=10){
+    console.log(tabuada, "x", mult, "= " + tabuada*mult);
     mult++;
+
 }  
